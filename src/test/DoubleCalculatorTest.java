81376173ac998578ac74bed0c48e59ffd4709f97
matei <ilismatei@gmail.com>
2026-04-07 08:54:12 +0300

    lab6

---
+++ src/test/DoubleCalculatorTest.java
@@ -0,0 +1,13 @@
+public class DoubleCalculatorTest {
+    public void testAddPositive() {...}
+    public void testAddNegatives() {...}
+    public void testSubtractPositives() {...}
+    public void testSubtractNegatives() {...}
+    public void testMultiplyPositives() {...}
+    public void testMultiplyNegatives() {...}
+    public void testMultiplyBy0() {...}
+    public void testDividePositives() {...}
+    public void testDivideNegatives() {...}
+    public void testDivideBy0() {...}
+
+}
---
+++ src/test/NewIntCalculatorTest.java
@@ -0,0 +1,12 @@
+public class NewIntCalculatorTest {
+    public void testAddPositive() {...}
+    public void testAddNegatives() {...}
+    public void testSubtractPositives() {...}
+    public void testSubtractNegatives() {...}
+    public void testMultiplyPositives() {...}
+    public void testMultiplyNegatives() {...}
+    public void testMultiplyBy0() {...}
+    public void testDividePositives() {...}
+    public void testDivideNegatives() {...}
+    public void testDivideBy0() {...}
+}
